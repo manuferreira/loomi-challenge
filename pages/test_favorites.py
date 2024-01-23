cfb63f2a1079cc94fc248cbee01ac8e815025110
manuferreira <manussa.fsantos@gmail.com>
2024-01-23 13:24:48 -0300

    Add the tests related to the favorites

---
+++ pages/test_favorites.py
@@ -0,0 +1,38 @@
+import unittest
+from selenium.webdriver import Chrome
+from pages.login_page import Login
+from config.Config import Config
+from pages.favorites_page import FavoritesPage
+
+class TestFavorites(unittest.TestCase):
+    def setUp(self):
+        self.webdriver = Chrome()
+        self.webdriver.maximize_window()
+        self.login_page = Login(self.webdriver)
+        self.favorites_page = FavoritesPage(self.webdriver)
+        self.favorite_team_name = ["Sport Recife", "Palmeiras"]
+        self.login_page.go_to_kasa()
+        self.login_page.login_button_click()
+        self.login_page.login(email=Config.EMAIL, password=Config.PASSWORD)
+
+    def tearDown(self):
+        self.favorites_page.remove_teams()
+        self.webdriver.quit()
+
+    def test_add_favorite_team(self):
+        self.login_page.go_to_favorites()
+        self.favorites_page.select_favorite_team(team_name=self.favorite_team_name[0])
+        self.favorites_page.select_favorite_team(team_name=self.favorite_team_name[1])
+
+    def test_check_favorite_team(self):
+        self.login_page.go_to_favorites()
+        self.favorites_page.select_favorite_team(team_name=self.favorite_team_name[0])
+        self.favorites_page.select_favorite_team(team_name=self.favorite_team_name[1])
+
+        favorite_team = self.favorites_page.get_favorite_team()
+        for team in favorite_team:
+            self.assertEqual(self.favorite_team_name, team, "Different results")
+
+
+if __name__ == '__main__':
+    unittest.main()
