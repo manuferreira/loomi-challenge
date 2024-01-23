8785526624444df8345e12cac4d64f80ae694995
manuferreira <manussa.fsantos@gmail.com>
2024-01-23 13:24:05 -0300

    Add configuration file

--- config/config.py
+++ config/config.py
@@ -1,7 +1,6 @@
-
 class Config:
     EMAIL = 'test.username0102@gmail.com'
     PASSWORD = 'xi9O@540'
     BASE_URL = 'https://www.kasa.live/'
     PAGE_LOAD_MAX = 5
-    ELEMENT_PAGE_MAX = 5
+    ELEMENT_PAGE_MAX = 10
