014e236a6b8b1ba0bb7d3e164720e81d27a22acf
manuferreira <manussa.fsantos@gmail.com>
2024-01-23 13:25:07 -0300

    Add the tests related to the highlights

---
+++ pages/test_highlights.py
@@ -0,0 +1,32 @@
+import unittest
+from selenium.webdriver import Chrome
+from config.Config import Config
+from pages.login_page import Login
+from pages.highlights_page import HighlightsPage
+
+
+class TestHighlights(unittest.TestCase):
+    def setUp(self):
+        self.webdriver = Chrome()
+        self.webdriver.maximize_window()
+        self.login_page = Login(self.webdriver)
+        self.highlights_page = HighlightsPage(self.webdriver)
+        self.team = "Sport Recife"
+
+    def tearDown(self):
+        self.webdriver.quit()
+
+    def test_assert_team_highlights(self):
+        self.login_page.go_to_kasa()
+        self.login_page.login_button_click()
+        self.login_page.login(email=Config.EMAIL, password=Config.PASSWORD)
+        self.login_page.go_to_moments()
+
+        self.highlights_page.filter_by_team(team_name=self.team)
+
+        teams = self.highlights_page.get_team_highlights()
+        if self.team not in teams:
+            raise AssertionError(f"{self.team} not found on: {teams}")
+
+if __name__ == '__main__':
+    unittest.main()
